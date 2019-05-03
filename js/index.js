934d5495bc1e2e1f3714f12a3812f1929b0949e5
Alok Shenoy <koyanloshe@gmail.com>
2019-05-03 19:59:10 +0530

    moved disclaimer file to a different folder

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 var siteUrl = window.location.href;
 
 var baseUrl = siteUrl.split('/');
-if (baseUrl.indexOf('#')){
-  baseUrl = baseUrl.slice(0, baseUrl.indexOf('#'));
+if (baseUrl.indexOf('#')) {
+    baseUrl = baseUrl.slice(0, baseUrl.indexOf('#'));
 }
-baseUrl = baseUrl.join('/')+'/';
+baseUrl = baseUrl.join('/') + '/';
 
 var endPoint = siteUrl.split('/');
 endPoint = endPoint[endPoint.length - 1]
@@ -61,34 +61,34 @@ var siteMap = {
 };
 
 // getPage(pagePath)
-function getPage(pagePath, key){
-  var request = new XMLHttpRequest();
-  request.open('GET', pagePath, true);
+function getPage(pagePath, key) {
+    var request = new XMLHttpRequest();
+    request.open('GET', pagePath, true);
 
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      var resp = request.responseText;
-      payLoad.pages[key] = resp;
-      localStorage.setItem(key, resp)
-    } else {
-      // We reached our target server, but it returned an error
-      console.log('there was an error');
-    }
-  };
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var resp = request.responseText;
+            payLoad.pages[key] = resp;
+            localStorage.setItem(key, resp)
+        } else {
+            // We reached our target server, but it returned an error
+            console.log('there was an error');
+        }
+    };
 
-  request.onerror = function() {
-    // There was a connection error of some sort
-    console.log('there was an error');
-  };
+    request.onerror = function() {
+        // There was a connection error of some sort
+        console.log('there was an error');
+    };
 
-  request.send();
+    request.send();
 }
 
 // setPage(pagePath, id)
-function setPage(pagePath, id, key){
-  var append = localStorage.getItem(key);
-  document.getElementById(id).innerHTML =append ;
+function setPage(pagePath, id, key) {
+    var append = localStorage.getItem(key);
+    document.getElementById(id).innerHTML = append;
 }
 
 /*
@@ -97,10 +97,10 @@ function setPage(pagePath, id, key){
 function update(pagePath) {
 
     var identifier = pagePath.split('.html')[0];
-    if(identifier.includes('-')){
-      identifier = identifier.split('-')[0];
+    if (identifier.includes('-')) {
+        identifier = identifier.split('-')[0];
     } else {
-      identifier;
+        identifier;
     }
 
     document.title = siteMap[identifier].title;
@@ -117,50 +117,50 @@ function router(pagePath) {
     switch (pagePath) {
         case siteMap.index.url:
             {
-              console.log('yes that is index');
-              setPage('home-page-fragment.html', insertLocationId, 'home');
-              update(pagePath);
-              break;
+                console.log('yes that is index');
+                setPage('home-page-fragment.html', insertLocationId, 'home');
+                update(pagePath);
+                break;
             }
         case siteMap.overview.url:
             {
-              console.log('yes that is overview');
-              setPage('guide-page-fragment.html', insertLocationId, 'guide');
-              update(pagePath);
-              break;
+                console.log('yes that is overview');
+                setPage('guide-page-fragment.html', insertLocationId, 'guide');
+                update(pagePath);
+                break;
             }
         case siteMap.content.url:
             {
-              console.log('yes that is content');
-              setPage('guide-page-fragment.html', insertLocationId, 'guide');
-              update(pagePath);
-              break;
+                console.log('yes that is content');
+                setPage('guide-page-fragment.html', insertLocationId, 'guide');
+                update(pagePath);
+                break;
             }
         case siteMap.design.url:
             {
-              console.log('yes that is design');
-              setPage('guide-page-fragment.html', insertLocationId, 'guide');
-              update(pagePath);
-              break;
+                console.log('yes that is design');
+                setPage('guide-page-fragment.html', insertLocationId, 'guide');
+                update(pagePath);
+                break;
             }
         case siteMap.components.url:
             {
-              console.log('yes that is components');
-              setPage('guide-page-fragment.html', insertLocationId, 'guide');
-              update(pagePath);
-              break;
+                console.log('yes that is components');
+                setPage('guide-page-fragment.html', insertLocationId, 'guide');
+                update(pagePath);
+                break;
             }
         default:
             {
-              console.log('yes that is default');
-              setPage('home-page-fragment.html', insertLocationId, 'home');
-              update('index');
-              break;
+                console.log('yes that is default');
+                setPage('home-page-fragment.html', insertLocationId, 'home');
+                update('index');
+                break;
             }
     }
 
     setTimeout(function(e) {
-      equalise();
+        equalise();
     }, 200)
 
 }
@@ -168,17 +168,17 @@ function router(pagePath) {
 // event listeners
 window.addEventListener('load', function(e) {
     equalise();
-    Object.keys(siteMap).forEach(function(each){
+    Object.keys(siteMap).forEach(function(each) {
         getPage(siteMap[each].url, each)
     })
 
-    var interval = setInterval(function(e){
-      if(localStorage.length === Object.keys(siteMap).length){
-        clearInterval(interval)
-        router(siteMap.home.url);
-        setPage('navigation.html', 'navigation-insert', 'navigation');
-        setPage('footer.html', 'footer-insert', 'footer');
-      }
+    var interval = setInterval(function(e) {
+        if (localStorage.length === Object.keys(siteMap).length) {
+            clearInterval(interval)
+            router(siteMap.home.url);
+            setPage('navigation.html', 'navigation-insert', 'navigation');
+            setPage('footer.html', 'footer-insert', 'footer');
+        }
 
-    },200)
+    }, 200)
 });
